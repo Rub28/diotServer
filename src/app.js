bb234c5439945df705af9a5914d779bdd226098a
Rub28 <ruhernandezm@gmail.com>
2026-01-19 21:20:08 -0600

    Se realizan cambio para definir el puerto a utilizar

--- src/app.js
+++ src/app.js
@@ -69,16 +69,17 @@ app.use(error);
    
 // Cargar los certificados de Let's Encrypt
 // solo para produccion. 
-/* 
+
 const options = {
     key: fs.readFileSync(path.join('/etc/letsencrypt/live/srv743626.hstgr.cloud/privkey.pem')),
     cert: fs.readFileSync(path.join('/etc/letsencrypt/live/srv743626.hstgr.cloud/fullchain.pem')),
   };
   
+
   // Crear el servidor HTTPS
-  https.createServer(options, app).listen(4005,'0.0.0.0', () => {
-    console.log('Servidor HTTPS escuchando en el puerto 4005');
+  https.createServer(options, app).listen(4006,'0.0.0.0', () => {
+    console.log('Servidor HTTPS escuchando en el puerto 4006');
   }); 
- */ 
+ 
 
 module.exports =app;
